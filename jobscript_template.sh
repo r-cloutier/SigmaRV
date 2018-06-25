776f22ce1e848ad5126a92a8bdc850a4ddd3cad1
Ryan Cloutier <cloutier@astro.utoronto.ca>
2018-06-25 12:25:10 -0400

    added new jobscript file

---
+++ jobscript_template.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#SBATCH --account=def-kristen
+#SBATCH --time=48:00:00
+#SBATCH --job-name=SigmaRV_<<band_str>>_<<R>>_<<Teff>>_<<logg>>
+python2.7 sigmaRV_grid.py <<band_str>> <<R>> <<Teff>> <<logg>>
