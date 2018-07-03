b115a9413640692704f65892471ef66d0ba166f5
Ryan Cloutier <cloutier@astro.utoronto.ca>
2018-07-03 12:23:44 +0100

    updated plots and add some filtered band tables for the rvfc

--- plotting.py
+++ plotting.py
@@ -7,10 +7,10 @@ global band_strs, Nband, NR, NTeff, Nlogg, NZ, Nvsini
 band_strs = ['U','B','V','R','I','Y','J','H','K']
 Nband, NR, NTeff, Nlogg, NZ, Nvsini = 9, 8, 68, 9, 9, 7
 
-cbticks = np.append(np.append(np.arange(.5,1,.1),np.arange(1,10,1)),
-                    np.arange(10,101,10))
+cbticks = np.append(np.append(np.append(np.arange(.3,1,.1),np.arange(1,10,1)),
+                    			np.arange(10,100,10)),np.arange(100,301,100))
 cbticklabels = np.repeat('', cbticks.size).astype('|S3')
-cbticklabels[np.array([5,7,14,16,23])] = ['1','3','10','30','100']
+cbticklabels[np.array([0,7,9,16,18,25,27])] = ['0.3','1','3','10','30','100','300']
 
 
 
@@ -67,7 +67,7 @@ def get_oneband(band_str):
 
 
 
-def plot_grid(pltt=True, label=False, vmin=.5, vmax=5e2):
+def plot_grid(pltt=True, label=False, vmin=.3, vmax=3e2):
     # define grids
     Rs = np.arange(20e3, 161e3, 2e4)
     Teffs = np.append(np.arange(28e2,7e3,1e2), np.arange(7e3,121e2,2e2))
@@ -103,11 +103,11 @@ def plot_grid(pltt=True, label=False, vmin=.5, vmax=5e2):
             axr.set_xticklabels('')
                 
             if i == 0:
-                cbar_axes = fig.add_axes([.25,.08,.5,.03])
+                cbar_axes = fig.add_axes([.2,.08,.6,.03])
                 cbar = fig.colorbar(cax, cax=cbar_axes, ticks=cbticks,
                                     orientation='horizontal')
                 cbar.set_label('$\sigma_{RV}$ [m s$^{-1}$]', labelpad=-1)
-                cbar.ax.set_xticklabels(cbticklabels)
+                cbar.ax.set_xticklabels(cbticklabels, fontsize=11)
                 
             # plot Teff v logg
             axl = fig.add_subplot(4,Nband,i+10)
@@ -151,7 +151,7 @@ def plot_grid(pltt=True, label=False, vmin=.5, vmax=5e2):
                 axv.set_ylabel('v$_s$sini$_s$\n[km s$^{-1}$]', fontsize=10)
             elif i == 4:
                 axv.set_yticklabels('')
-                axv.text(-.75, -.4, 'T$_{eff}$ x 10$^{-3}$\t[K]', fontsize=12,
+                axv.text(-.68, -.4, 'T$_{eff}$ x 10$^{-3}$\t[K]', fontsize=12,
                          transform=axv.transAxes)
             else:
                 axv.set_yticklabels('')
@@ -163,7 +163,7 @@ def plot_grid(pltt=True, label=False, vmin=.5, vmax=5e2):
         except IOError:
             pass
     
-    fig.subplots_adjust(left=.09, bottom=.2, top=.95, right=.99, hspace=.07, wspace=.07)
+    fig.subplots_adjust(left=.095, bottom=.2, top=.95, right=.99, hspace=.1, wspace=.1)
     if label:
         plt.savefig('plots/sigRV_grid.png')
     if pltt:
@@ -172,7 +172,7 @@ def plot_grid(pltt=True, label=False, vmin=.5, vmax=5e2):
 
 
 
-def reduce_dimension(arr, axes, sigma=0):
+def reduce_dimension(arr, axes, sigma=.7):
     axes = np.sort(list(axes))[::-1]
     for i in range(len(axes)):        
         arr = np.nanmedian(arr, axis=axes[i])
